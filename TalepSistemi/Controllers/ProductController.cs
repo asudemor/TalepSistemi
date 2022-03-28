5852927b75a25034e0b65ee7bc1f8edbbb40390b
asudemor <asude-bjk@hotmail.com>
2022-03-28 13:27:26 +0300

    Add project files.

---
+++ TalepSistemi/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TalepSistemi.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //localhost:39466/product/list
+        public string list()
+        {
+            return "product/list";
+        }
+        //localhost:39466/product/details
+        public string details()
+        {
+            return "product/details";
+        }
+    }
+}
