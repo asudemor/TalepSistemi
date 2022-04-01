00f14400a5bd6ceedab024690a5739f423db4b88
asudemor <asude-bjk@hotmail.com>
2022-04-01 10:31:43 +0300

    Servis katmanı oluşturuldu.

---
+++ Service/Concrate/AuthService.cs
@@ -0,0 +1,13 @@
+﻿using Service.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Concrate
+{
+    public class AuthService:IAuthService
+    {
+    }
+}
